c5a875054f46f4e3926ee60cd8476647b9ff1834
Somkiat Puisungnoen <somkiat.p@gmail.com>
2013-09-07 01:11:03 +0700

    Modify service of mongodb

--- 08-mongodb/service_person.js
+++ 08-mongodb/service_person.js
@@ -5,7 +5,7 @@ Db = mongo.Db,
 BSON = mongo.BSONPure;
  
 var server = new Server('localhost', 27017, {auto_reconnect: true});
-db = new Db('winedb', server);
+db = new Db('persons', server);
  
 db.open(function(err, db) {
     if(!err) {
