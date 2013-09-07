9032460e0b74147c45456ad881d8a814b3527cee
Somkiat Puisungnoen <somkiat.p@gmail.com>
2013-09-07 14:47:48 +0700

    Update service person of mysql

--- 06-mysql/service_person.js
+++ 06-mysql/service_person.js
@@ -1,8 +1,8 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection(
     {   host: 'localhost', 
-        user: 'root',  
-        password: '42112121', 
+        user: '<username>',  
+        password: '<password>', 
         database: 'demo_nodejs'
     }
 );
